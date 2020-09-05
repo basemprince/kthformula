8e509fab5c2c6b0bf37c8799a6bdc40188819eb5
basemprince <bshaker@kth.se>
2020-09-05 09:37:44 +0200

    Third commit

--- src/exc2/Visualisation.py
+++ src/exc2/Visualisation.py
@@ -3,20 +3,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 import math
-
-
 plt.style.use('ggplot')
 
+# arrays to accumulate time and function results
 current_array = []
 func_array = []
+
+# the time when the script started
 start_time = time.time()
-while True:
+# signal to terminate the script if the plot was closed
+go_signal = True
+
+while True and go_signal:
     current_time = time.time() - start_time
     t = 5 * math.sin ( 2 * math.pi * 1 * current_time )
     func = 3 * math.pi * math.exp(-t) 
     current_array.append(current_time)
     func_array.append(func)
-    print(current_time)
     if(len(current_array)>20):
         current_array.pop(0)
         func_array.pop(0)
@@ -25,7 +28,7 @@ while True:
     plt.xlabel ('Time')
     plt.ylabel ('h ( t )')
     plt.title ('h ( t ) = 3 * pi * exp(-lambda [ t ] )')
-    plt.grid()
     plt.pause(0.01)
+    go_signal = plt.get_fignums()
 
 plt.show()
